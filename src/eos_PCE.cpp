8cad5afd2ebef4f492c6d29a6b2c7fc324c4a3ae
Andrew Gordeev <agoragordeev@ucla.edu>
2021-06-24 14:59:27 -0400

    Use e^0.25 spacing

--- src/eos_PCE.cpp
+++ src/eos_PCE.cpp
@@ -68,8 +68,8 @@ void EOS_PCE::initialize_eos() {
 	cs2_tb[itable] = Util::mtx_malloc(nb_length[itable], e_length[itable]);
         double temp;
         for (int ii = 0; ii < e_length[itable]; ii++) {
-	    eos_file->read((char*)&temp, sizeof(double));  // e
-            temp /= Util::hbarc;      // 1/fm^4
+	    eos_file->read((char*)&temp, sizeof(double));  // e^(1/4)
+            //temp /= Util::hbarc;      // 1/fm
             if (ii == 0) e_bounds[itable] = temp;
             if (ii == 1) e_spacing[itable] = temp - e_bounds[itable];
             if (ii == e_length[itable] - 1) set_eps_max(temp);
@@ -99,20 +99,21 @@ double EOS_PCE::p_e_func(double e, double rhob, double proper_tau) const {
 //! input local energy density eps [1/fm^4] and rhob [1/fm^3]
 double EOS_PCE::get_temperature(double e, double rhob, double proper_tau) const {
     double T_QCD;
-    if (e < 0.1) {  // This corresponds to T < ~0.1 GeV
-        T_QCD = (0.18175818963347465 * std::pow(e*Util::hbarc, 0.1433156325035367))/Util::hbarc;
-    }
-    else T_QCD = interpolate1D(e, 0, temperature_tb);  // 1/fm
-    // double T_QCD = interpolate1D(e, 0, temperature_tb);  // 1/fm
+    //   if (e < 0.1) {  // This corresponds to T < ~0.1 GeV
+    //    T_QCD = (0.18175818963347465 * std::pow(e*Util::hbarc, 0.1433156325035367))/Util::hbarc;
+    //}
+    //else T_QCD = interpolate1D(e, 0, temperature_tb);  // 1/fm
+    T_QCD = interpolate1D(std::pow(e,0.25), 0, temperature_tb);  // 1/fm
     double T_gluon;
-    if (e < 0.1) {  // This corresponds to T < ~0.2 GeV
-        T_gluon = (0.545827055067574 * std::pow(e*Util::hbarc, 0.2237373605050102))/Util::hbarc;
-    }
-    else T_gluon = interpolate1D(e, 1, temperature_tb);
+    //if (e < 0.1) {  // This corresponds to T < ~0.2 GeV
+    //   T_gluon = (0.545827055067574 * std::pow(e*Util::hbarc, 0.2237373605050102))/Util::hbarc;
+    //}
+    //else T_gluon = interpolate1D(e, 1, temperature_tb);
+    T_gluon = interpolate1D(std::pow(e,0.25), 1, temperature_tb);
     double fugacity = get_fugacity(proper_tau);
     double T = fugacity * T_QCD + (1 - fugacity) * T_gluon;
 
-    T = (.15274608127810668 * std::pow(e*Util::hbarc, .2478330589996049))/Util::hbarc;
+    //T = (.15274608127810668 * std::pow(e*Util::hbarc, .2478330589996049))/Util::hbarc;
     return(std::max(1e-15, T));
 }
 
@@ -121,12 +122,12 @@ double EOS_PCE::get_temperature(double e, double rhob, double proper_tau) const 
 //! the input local energy density [1/fm^4], rhob [1/fm^3]
 double EOS_PCE::get_pressure(double e, double rhob, double proper_tau) const {
   // double T = get_temperature(e, rhob, proper_tau);
-    double f_QCD = interpolate1D(e, 0, pressure_tb);  // 1/fm^4
-    double f_gluon = interpolate1D(e, 1, pressure_tb);
+    double f_QCD = interpolate1D(std::pow(e,0.25), 0, pressure_tb);  // 1/fm^4
+    double f_gluon = interpolate1D(std::pow(e,0.25), 1, pressure_tb);
     double fugacity = get_fugacity(proper_tau);
     double f = fugacity * f_QCD + (1 - fugacity) * f_gluon;
 
-    f = (-.0000000775447 * std::pow(e*Util::hbarc,0.247833) + 0.329492 * e*Util::hbarc)/Util::hbarc;
+    //f = (-.0000000775447 * std::pow(e*Util::hbarc,0.247833) + 0.329492 * e*Util::hbarc)/Util::hbarc;
     return(std::max(1e-15, f));
 }
 
@@ -146,7 +147,7 @@ double EOS_PCE::get_fugacity(double proper_tau) const {
     double tau0 = 0.0;
     double tau_eq = 5.0;
     if (tau0 <= 0 or tau_eq <= 0) {
-        fugacity = 1;
+        fugacity = 0;
     }
     else {
         fugacity = 1 - exp((tau0 - proper_tau)/tau_eq);
@@ -163,12 +164,12 @@ double EOS_PCE::get_cs2(double e, double rhob, double proper_tau) const {
 	//  	std::cout << "cs2 debug: " << e << " " << T << " " << cs2_QCD << std::endl;
     //}
     //else
-    cs2_QCD = interpolate1D(e, 0, cs2_tb);  // 1/fm
-    double cs2_gluon = interpolate1D(e, 1, cs2_tb);
+    cs2_QCD = interpolate1D(std::pow(e,0.25), 0, cs2_tb);  // 1/fm
+    double cs2_gluon = interpolate1D(std::pow(e,0.25), 1, cs2_tb);
     double fugacity = get_fugacity(proper_tau);
     double cs2 = fugacity * cs2_QCD + (1 - fugacity) * cs2_gluon;
-    double T = interpolate1D(e,0,temperature_tb)*Util::hbarc;
-    if (T < 0.18410179) cs2 = 246.0490286078087 * std::pow(T,3) - 83.01937867454492 * std::pow(T,2) + 7.936821067345355 * T - 0.00025794218162239854;
-    else cs2 = 1/3. - 0.008183471854138312/(T - 0.12997412301945538);
+    //double T = interpolate1D(e,0,temperature_tb)*Util::hbarc;
+    //if (T < 0.18410179) cs2 = 246.0490286078087 * std::pow(T,3) - 83.01937867454492 * std::pow(T,2) + 7.936821067345355 * T - 0.00025794218162239854;
+    //else cs2 = 1/3. - 0.008183471854138312/(T - 0.12997412301945538);
     return(std::max(1e-15, cs2));
 }
