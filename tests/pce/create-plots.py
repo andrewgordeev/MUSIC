ecd5ef673ac06866f473a994e8842ffa2fccb241
Andrew Gordeev <agoragordeev@ucla.edu>
2021-07-13 18:03:58 -0400

    Speed up plotting and clean up filenames

--- tests/pce/create-plots.py
+++ tests/pce/create-plots.py
@@ -12,54 +12,47 @@ from scipy.interpolate import griddata
 
 results_file = '../../evolution_xyeta.dat'
 
-tvals = np.array([])
-xvals = np.array([])
-yvals = np.array([])
-tempvals = np.array([])
-evals = np.array([])
-vvals = np.array([])
-Tpropvals = np.array([])
-pvals = np.array([])
-vxvals = np.array([])
-vyvals = np.array([])
-vzvals = np.array([])
-
 xmax = 15.0
 ymax = 15.0
 t0 = 0.4
-tmax = t0+0.025
+tmax = t0+0.0025
 dx = 0.1
 dy = 0.1
-dt = 0.025*1
+dt = 0.0025
+nx = int(2*xmax/dx)
+ny = int(2*ymax/dy)
+nt = int((tmax-t0)/dt + 1)
 
+tvals = np.array([])
+yvals = np.array([])
 
-with open(results_file, 'r') as f:
-    for t in np.arange(t0, tmax+dt, dt):
-        if t > tmax:
-            break
-        print("Reading timestep: " + str(t) + ' fm')
-        for y in np.arange(-ymax, ymax, dy):
-            for x in np.arange(-xmax, xmax, dx):
-                line = f.readline()
-                tvals = np.append(tvals, t)
-                xvals = np.append(xvals, x)
-                yvals = np.append(yvals, y)
-                tempvals = np.append(tempvals, 1000*float(line.split()[0]))
-                vxvals = np.append(vxvals, float(line.split()[2]))
-                vyvals = np.append(vyvals, float(line.split()[3]))
-                vzvals = np.append(vzvals, float(line.split()[4]))
-    f.close()
-    
+for t in np.arange(t0, tmax+dt, dt):
+    if t > tmax:
+        break
+    tvals = np.append(tvals, np.repeat(t,nx*ny))
+
+for y in np.arange(-ymax, ymax, dy):
+    yvals = np.append(yvals, np.repeat(y, nx))
+yvals = np.tile(yvals, nt)
+        
+xvals = np.tile(np.arange(-xmax,xmax,dx),ny*nt)
+
+results = np.loadtxt(results_file)
+tempvals = 1000 * results[:,0]
+vxvals = results[:,2]
+vyvals = results[:,3]
+vzvals = results[:,4]
+   
 rvals = np.sqrt(xvals**2 + yvals**2)
 vvals = np.sqrt(vxvals**2 + vyvals**2)
 
 """ Filter to get one slice of y, t values """
 
-grid_step = 0.15
-step_fraction = 0.1
+grid_step = 0.1
+step_fraction = 0.05
 xtarget = 0.0
 ytarget = 0.0
-ttarget = 0.425
+ttarget = 0.4025
 
 newyvals = yvals[abs(yvals-ytarget)<grid_step]
 newxvals = xvals[abs(yvals-ytarget)<grid_step]
@@ -70,13 +63,13 @@ newvxvals = vxvals[abs(yvals-ytarget)<grid_step]
 newvyvals = vyvals[abs(yvals-ytarget)<grid_step]
 newvzvals = vzvals[abs(yvals-ytarget)<grid_step]
 
-newtempvals = newtempvals[abs(newtvals-ttarget)<grid_step*step_fraction]
-newvvals = newvvals[abs(newtvals-ttarget)<grid_step*step_fraction]
-newvxvals = newvxvals[abs(newtvals-ttarget)<grid_step*step_fraction]
-newvyvals = newvyvals[abs(newtvals-ttarget)<grid_step*step_fraction]
-newvzvals = newvzvals[abs(newtvals-ttarget)<grid_step*step_fraction]
-newxvals = newxvals[abs(newtvals-ttarget)<grid_step*step_fraction]
-newtvals = newtvals[abs(newtvals-ttarget)<grid_step*step_fraction]
+# newtempvals = newtempvals[abs(newtvals-ttarget)<grid_step*step_fraction]
+# newvvals = newvvals[abs(newtvals-ttarget)<grid_step*step_fraction]
+# newvxvals = newvxvals[abs(newtvals-ttarget)<grid_step*step_fraction]
+# newvyvals = newvyvals[abs(newtvals-ttarget)<grid_step*step_fraction]
+# newvzvals = newvzvals[abs(newtvals-ttarget)<grid_step*step_fraction]
+# newxvals = newxvals[abs(newtvals-ttarget)<grid_step*step_fraction]
+# newtvals = newtvals[abs(newtvals-ttarget)<grid_step*step_fraction]
 
 """ Plot against Mathematica data """
 
@@ -104,5 +97,5 @@ mathematica_data = np.fromfile('mathematica_plot.dat').reshape(-1,2)
 plt.plot(mathematica_data[:,0],mathematica_data[:,1], c='g', label = 'Mathematica')
 plt.plot(-mathematica_data[:,0],mathematica_data[:,1], c='g')
 plt.legend()
-plt.title(r'$y = 0, \tau = 0.425 fm/c$')
+plt.title(r'$y = 0, \tau = 0.4025 fm/c$')
 plt.savefig('plots/VelocityComparison.png')
